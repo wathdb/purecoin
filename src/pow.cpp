0279349616e06e930cb202ae0253467c6ecdf0d3
wathD <114563307+wathdb@users.noreply.github.com>
2025-08-12 11:45:42 +0200

    Update pow.cpp

--- src/pow.cpp
+++ src/pow.cpp
@@ -1,7 +1,8 @@
-// Copyright (c) 2009-2010 Satoshi Nakamoto
-// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2025 The PureCoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 
 #include <pow.h>
 
@@ -14,37 +15,22 @@
 unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
 {
     assert(pindexLast != nullptr);
+
     unsigned int nProofOfWorkLimit = UintToArith256(params.powLimit).GetCompact();
 
-    // Only change once per difficulty adjustment interval
-    if ((pindexLast->nHeight+1) % params.DifficultyAdjustmentInterval() != 0)
-    {
-        if (params.fPowAllowMinDifficultyBlocks)
-        {
-            // Special difficulty rule for testnet:
-            // If the new block's timestamp is more than 2* 10 minutes
-            // then it MUST be a min-difficulty block.
-            if (pblock->GetBlockTime() > pindexLast->GetBlockTime() + params.nPowTargetSpacing*2)
-                return nProofOfWorkLimit;
-            else
-            {
-                // Return the last non-special-min-difficulty-rules-block
-                const CBlockIndex* pindex = pindexLast;
-                while (pindex->pprev && pindex->nHeight % params.DifficultyAdjustmentInterval() != 0 && pindex->nBits == nProofOfWorkLimit)
-                    pindex = pindex->pprev;
-                return pindex->nBits;
-            }
-        }
-        return pindexLast->nBits;
+    // Temps entre le dernier bloc et celui d'avant
+    int64_t lastBlockTime = pindexLast->GetBlockTime();
+    int64_t prevBlockTime = pindexLast->pprev ? pindexLast->pprev->GetBlockTime() : lastBlockTime;
+
+    int64_t actualSpacing = lastBlockTime - prevBlockTime;
+
+    // Si plus de 180 secondes, difficulté minimale (plus facile)
+    if (actualSpacing > 180) {
+        return nProofOfWorkLimit;
     }
 
-    // Go back by what we want to be 14 days worth of blocks
-    int nHeightFirst = pindexLast->nHeight - (params.DifficultyAdjustmentInterval()-1);
-    assert(nHeightFirst >= 0);
-    const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
-    assert(pindexFirst);
-
-    return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
+    // Sinon on garde la difficulté actuelle
+    return pindexLast->nBits;
 }
 
 unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
@@ -65,9 +51,6 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
 
     // Special difficulty rule for Testnet4
     if (params.enforce_BIP94) {
-        // Here we use the first block of the difficulty period. This way
-        // the real difficulty is always preserved in the first block as
-        // it is not allowed to use the min-difficulty exception.
         int nHeightFirst = pindexLast->nHeight - (params.DifficultyAdjustmentInterval()-1);
         const CBlockIndex* pindexFirst = pindexLast->GetAncestor(nHeightFirst);
         bnNew.SetCompact(pindexFirst->nBits);
@@ -84,8 +67,6 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
     return bnNew.GetCompact();
 }
 
-// Check that on difficulty adjustments, the new difficulty does not increase
-// or decrease beyond the permitted limits.
 bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t height, uint32_t old_nbits, uint32_t new_nbits)
 {
     if (params.fPowAllowMinDifficultyBlocks) return true;
@@ -108,8 +89,6 @@ bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t heig
             largest_difficulty_target = pow_limit;
         }
 
-        // Round and then compare this new calculated value to what is
-        // observed.
         arith_uint256 maximum_new_target;
         maximum_new_target.SetCompact(largest_difficulty_target.GetCompact());
         if (maximum_new_target < observed_new_target) return false;
@@ -124,8 +103,6 @@ bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t heig
             smallest_difficulty_target = pow_limit;
         }
 
-        // Round and then compare this new calculated value to what is
-        // observed.
         arith_uint256 minimum_new_target;
         minimum_new_target.SetCompact(smallest_difficulty_target.GetCompact());
         if (minimum_new_target > observed_new_target) return false;
@@ -135,8 +112,6 @@ bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t heig
     return true;
 }
 
-// Bypasses the actual proof of work check during fuzz testing with a simplified validation checking whether
-// the most significant bit of the last byte of the hash is set.
 bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params& params)
 {
     if (EnableFuzzDeterminism()) return (hash.data()[31] & 0x80) == 0;
@@ -151,7 +126,6 @@ std::optional<arith_uint256> DeriveTarget(unsigned int nBits, const uint256 pow_
 
     bnTarget.SetCompact(nBits, &fNegative, &fOverflow);
 
-    // Check range
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > UintToArith256(pow_limit))
         return {};
 
@@ -163,7 +137,6 @@ bool CheckProofOfWorkImpl(uint256 hash, unsigned int nBits, const Consensus::Par
     auto bnTarget{DeriveTarget(nBits, params.powLimit)};
     if (!bnTarget) return false;
 
-    // Check proof of work matches claimed amount
     if (UintToArith256(hash) > bnTarget)
         return false;
 
