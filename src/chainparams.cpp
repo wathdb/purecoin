50e1c20aefca3abb5c7c6f1fafef3730a3da44ff
wathD <114563307+wathdb@users.noreply.github.com>
2025-08-12 11:15:03 +0200

    Update chainparams.cpp
    Initial commit of PureCoin genesis parameters:
    - Created genesis block with custom timestamp and nonce
    - Set genesis reward to 50 coins sent to specified P2WPKH address
    - Configured network parameters: block time 45s, block size limits, difficulty target
    - Disabled SegWit activation
    - Updated message start bytes and network ports
    - Added custom base58 and bech32 prefixes for PureCoin
    - Set copyright to PureCoin Core developers (2025)

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -1,111 +1,105 @@
 // Copyright (c) 2010 Satoshi Nakamoto
-// Copyright (c) 2009-2022 The Bitcoin Core developers
+// Copyright (c) 2025 The PureCoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <chainparams.h>
-
-#include <chainparamsbase.h>
-#include <common/args.h>
 #include <consensus/params.h>
-#include <deploymentinfo.h>
-#include <logging.h>
+#include <arith_uint256.h>
 #include <tinyformat.h>
-#include <util/chaintype.h>
-#include <util/strencodings.h>
-#include <util/string.h>
+#include <util/system.h>
+#include <assert.h>
+#include <chainparamsseeds.h>
 
-#include <cassert>
-#include <cstdint>
-#include <limits>
-#include <stdexcept>
-#include <vector>
-
-using util::SplitString;
-
-void ReadSigNetArgs(const ArgsManager& args, CChainParams::SigNetOptions& options)
+static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    if (!args.GetArgs("-signetseednode").empty()) {
-        options.seeds.emplace(args.GetArgs("-signetseednode"));
-    }
-    if (!args.GetArgs("-signetchallenge").empty()) {
-        const auto signet_challenge = args.GetArgs("-signetchallenge");
-        if (signet_challenge.size() != 1) {
-            throw std::runtime_error("-signetchallenge cannot be multiple values.");
-        }
-        const auto val{TryParseHex<uint8_t>(signet_challenge[0])};
-        if (!val) {
-            throw std::runtime_error(strprintf("-signetchallenge must be hex, not '%s'.", signet_challenge[0]));
-        }
-        options.challenge.emplace(*val);
-    }
+    const char* pszTimestamp = "PureCoin genesis block - 2025-08-12";
+    const CScript genesisOutputScript = CScript() << OP_0 << ParseHex("3291c836314c9b704710c0e057931f1c3f9a242c");
+
+    CMutableTransaction txNew;
+    txNew.nVersion = 1;
+    txNew.vin.resize(1);
+    txNew.vout.resize(1);
+    txNew.vin[0].scriptSig = CScript() << 486604799 << CScriptNum(4) 
+        << std::vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
+    txNew.vout[0].nValue = genesisReward;
+    txNew.vout[0].scriptPubKey = genesisOutputScript;
+
+    CBlock genesis;
+    genesis.nTime    = nTime;
+    genesis.nBits    = nBits;
+    genesis.nNonce   = nNonce;
+    genesis.nVersion = nVersion;
+    genesis.vtx.push_back(MakeTransactionRef(std::move(txNew)));
+    genesis.hashPrevBlock.SetNull();
+    genesis.hashMerkleRoot = BlockMerkleRoot(genesis);
+    return genesis;
 }
 
-void ReadRegTestArgs(const ArgsManager& args, CChainParams::RegTestOptions& options)
-{
-    if (auto value = args.GetBoolArg("-fastprune")) options.fastprune = *value;
-    if (HasTestOption(args, "bip94")) options.enforce_bip94 = true;
+class CMainParams : public CChainParams {
+public:
+    CMainParams() {
+        strNetworkID = "purecoin-main";
 
-    for (const std::string& arg : args.GetArgs("-testactivationheight")) {
-        const auto found{arg.find('@')};
-        if (found == std::string::npos) {
-            throw std::runtime_error(strprintf("Invalid format (%s) for -testactivationheight=name@height.", arg));
-        }
+        pchMessageStart[0] = 0xa1;
+        pchMessageStart[1] = 0xb2;
+        pchMessageStart[2] = 0xc3;
+        pchMessageStart[3] = 0xd4;
 
-        const auto value{arg.substr(found + 1)};
-        const auto height{ToIntegral<int32_t>(value)};
-        if (!height || *height < 0 || *height >= std::numeric_limits<int>::max()) {
-            throw std::runtime_error(strprintf("Invalid height value (%s) for -testactivationheight=name@height.", arg));
-        }
+        nDefaultPort = 28333;
+        nPruneAfterHeight = 100000;
 
-        const auto deployment_name{arg.substr(0, found)};
-        if (const auto buried_deployment = GetBuriedDeployment(deployment_name)) {
-            options.activation_heights[*buried_deployment] = *height;
-        } else {
-            throw std::runtime_error(strprintf("Invalid name (%s) for -testactivationheight=name@height.", arg));
-        }
+        consensus.nSubsidyHalvingInterval = 210000;
+        consensus.nPowTargetSpacing = 45; // 45 seconds per block
+
+        consensus.BIP16Exception = uint256S("0x0");
+        consensus.BIP34Height = 0;
+        consensus.BIP34Hash = uint256();
+        consensus.BIP65Height = 0;
+        consensus.BIP66Height = 0;
+        consensus.CSVHeight = 0;
+        consensus.SegwitHeight = std::numeric_limits<int>::max(); // Disable Segwit
+
+        consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+
+        consensus.nRuleChangeActivationThreshold = 1815; // 90% of 2016
+        consensus.nMinerConfirmationWindow = 2016;
+
+        consensus.nMaxBlockWeight = 500000; 
+        consensus.nMaxSerializedBlockSize = 500000;
+
+        genesis = CreateGenesisBlock(1691800000, 214878, 0x1e0ffff0, 1, 50 * COIN);
+        consensus.hashGenesisBlock = genesis.GetHash();
+
+        assert(consensus.hashGenesisBlock == uint256S("0x000007029b560c8b619ca32b1685d75ea8dfa884730180faa45bf7d2e4c0b9b9"));
+        assert(genesis.hashMerkleRoot == uint256S("0xd131e7aaf25cc7c7bb5a2b18bc235a6a835f6d70e864ab6b7f28b934c1ec9d2c"));
+
+        vSeeds.clear(); // Add seed nodes here if you have any
+
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,28); // Prefix 'P'
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,50);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
+
+        base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
+        base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
+
+        bech32_hrp = "pc";
+
+        fDefaultConsistencyChecks = false;
+        fRequireStandard = true;
+        m_is_test_chain = false;
+        m_is_mockable_chain = false;
     }
 
-    for (const std::string& strDeployment : args.GetArgs("-vbparams")) {
-        std::vector<std::string> vDeploymentParams = SplitString(strDeployment, ':');
-        if (vDeploymentParams.size() < 3 || 4 < vDeploymentParams.size()) {
-            throw std::runtime_error("Version bits parameters malformed, expecting deployment:start:end[:min_activation_height]");
-        }
-        CChainParams::VersionBitsParameters vbparams{};
-        const auto start_time{ToIntegral<int64_t>(vDeploymentParams[1])};
-        if (!start_time) {
-            throw std::runtime_error(strprintf("Invalid nStartTime (%s)", vDeploymentParams[1]));
-        }
-        vbparams.start_time = *start_time;
-        const auto timeout{ToIntegral<int64_t>(vDeploymentParams[2])};
-        if (!timeout) {
-            throw std::runtime_error(strprintf("Invalid nTimeout (%s)", vDeploymentParams[2]));
-        }
-        vbparams.timeout = *timeout;
-        if (vDeploymentParams.size() >= 4) {
-            const auto min_activation_height{ToIntegral<int64_t>(vDeploymentParams[3])};
-            if (!min_activation_height) {
-                throw std::runtime_error(strprintf("Invalid min_activation_height (%s)", vDeploymentParams[3]));
+    const Checkpoints::CCheckpointData& Checkpoints() const override {
+        static Checkpoints::CCheckpointData data {
+            {
+                {0, consensus.hashGenesisBlock}
             }
-            vbparams.min_activation_height = *min_activation_height;
-        } else {
-            vbparams.min_activation_height = 0;
-        }
-        bool found = false;
-        for (int j=0; j < (int)Consensus::MAX_VERSION_BITS_DEPLOYMENTS; ++j) {
-            if (vDeploymentParams[0] == VersionBitsDeploymentInfo[j].name) {
-                options.version_bits_parameters[Consensus::DeploymentPos(j)] = vbparams;
-                found = true;
-                LogInfo("Setting version bits activation parameters for %s to start=%ld, timeout=%ld, min_activation_height=%d",
-                        vDeploymentParams[0], vbparams.start_time, vbparams.timeout, vbparams.min_activation_height);
-                break;
-            }
-        }
-        if (!found) {
-            throw std::runtime_error(strprintf("Invalid deployment (%s)", vDeploymentParams[0]));
-        }
+        };
+        return data;
     }
-}
+};
 
 static std::unique_ptr<const CChainParams> globalChainParams;
 
@@ -116,25 +110,16 @@ const CChainParams &Params() {
 
 std::unique_ptr<const CChainParams> CreateChainParams(const ArgsManager& args, const ChainType chain)
 {
-    switch (chain) {
+    switch(chain) {
     case ChainType::MAIN:
-        return CChainParams::Main();
+        return std::make_unique<CMainParams>();
     case ChainType::TESTNET:
-        return CChainParams::TestNet();
-    case ChainType::TESTNET4:
-        return CChainParams::TestNet4();
-    case ChainType::SIGNET: {
-        auto opts = CChainParams::SigNetOptions{};
-        ReadSigNetArgs(args, opts);
-        return CChainParams::SigNet(opts);
+        throw std::runtime_error("PureCoin TESTNET not implemented");
+    case ChainType::REGTEST:
+        throw std::runtime_error("PureCoin REGTEST not implemented");
+    default:
+        throw std::runtime_error("Unknown chain");
     }
-    case ChainType::REGTEST: {
-        auto opts = CChainParams::RegTestOptions{};
-        ReadRegTestArgs(args, opts);
-        return CChainParams::RegTest(opts);
-    }
-    }
-    assert(false);
 }
 
 void SelectParams(const ChainType chain)
